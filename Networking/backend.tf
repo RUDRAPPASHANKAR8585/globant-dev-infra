524addbfc97bae0302c0efa6e4d21e29e07f3ff3
ganeshbabu <sganeshbabu316@gmail.com>
2025-05-08 11:24:09 +0530

    added infra files

--- Networking/backend.tf
+++ Networking/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     region            = "ap-south-1"
-    key               = "network/terraform.tfstate"
+    key               = "network/new/terraform.tfstate"
     bucket            = "globant-poc-terraform-statefile-ap-south-1"
     dynamodb_table    = "globant-eks-poc-terraformstate-lock"
     encrypt           = true
