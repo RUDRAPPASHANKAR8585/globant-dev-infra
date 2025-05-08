4205760e2aedefe402f0549d9c231a85728ae755
ganeshbabu <sganeshbabu316@gmail.com>
2025-05-08 13:05:15 +0530

    added infra files

--- Networking/local.tf
+++ Networking/local.tf
@@ -1,41 +1,40 @@
 
 locals {
 
-  prefix = "globant-chandrakala"
 
   subnet_map = {
-    "${local.prefix}-public1-subnet-az1" = {
-      name                    = "${local.prefix}-public1-subnet-az1"
+    "${namespace}-public1-subnet-az1" = {
+      name                    = "${namespace}-public1-subnet-az1"
       cidr_block              = "10.0.0.0/19"
       availability_zone       = "ap-south-1a"
-      nat_gateway_name        = "${local.prefix}-az1-ngtw01"
+      nat_gateway_name        = "${namespace}-az1-ngtw01"
       attach_nat_gateway      = false
       create_nat_gateway      = true
       attach_internet_gateway = true
     },
-    "${local.prefix}-public2-subnet-az2" = {
-      name                    = "${local.prefix}-public2-subnet-az2"
+    "${namespace}-public2-subnet-az2" = {
+      name                    = "${namespace}-public2-subnet-az2"
       cidr_block              = "10.0.32.0/19"
       availability_zone       = "ap-south-1b"
-      nat_gateway_name        = "${local.prefix}-az2-ngtw01"
+      nat_gateway_name        = "${namespace}-az2-ngtw01"
       attach_nat_gateway      = false
       create_nat_gateway      = true
       attach_internet_gateway = true
     },
-    "${local.prefix}-private1-subnet-az1" = {
-      name                    = "${local.prefix}-private1-subnet-az1"
+    "${namespace}-private1-subnet-az1" = {
+      name                    = "${namespace}-private1-subnet-az1"
       cidr_block              = "10.0.128.0/19"
       availability_zone       = "ap-south-1a"
-      nat_gateway_name        = "${local.prefix}-az1-ngtw01"
+      nat_gateway_name        = "${namespace}-az1-ngtw01"
       attach_nat_gateway      = true
       create_nat_gateway      = false
       attach_internet_gateway = false
     },
-    "${local.prefix}-private2-subnet-az2" = {
-      name                    = "${local.prefix}-private2-subnet-az2"
+    "${namespace}-private2-subnet-az2" = {
+      name                    = "${namespace}-private2-subnet-az2"
       cidr_block              = "10.0.160.0/19"
       availability_zone       = "ap-south-1b"
-      nat_gateway_name        = "${local.prefix}-az2-ngtw01"
+      nat_gateway_name        = "${namespace}-az2-ngtw01"
       attach_nat_gateway      = true
       create_nat_gateway      = false
       attach_internet_gateway = false
